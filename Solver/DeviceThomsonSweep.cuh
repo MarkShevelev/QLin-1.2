e519e92ae09091fbf8d2f38ee94ee22870a4dc8c
Mark Shevelev <lionishy@gmail.com>
2021-07-27 20:44:33 +0300

    Device Thomson Sweep template added

---
+++ Solver/DeviceThomsonSweep.cuh
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+namespace iki {
+	template<typename T>
+	__global__ void thomson_sweep_kernel() {
+
+	}
+}/*iki*/
