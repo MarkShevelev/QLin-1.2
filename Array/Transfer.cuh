2a08b0034b8c10d27de8cfe9f8e1ee62b69c3b02
Mark Shevelev <lionishy@gmail.com>
2021-07-28 17:33:53 +0300

    Transfer function opened

---
+++ Array/Transfer.cuh
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <Device/DeviceError.h>
+#include <Array/DeviceArray.cuh>
+#include <Array/HostArray.h>
+
+namespace iki {
+	template <typename T>
+	void transfer() {
+
+	}
+}/*iki*/
