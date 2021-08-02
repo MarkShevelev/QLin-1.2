291e73aeca14d1463c9d5e018dced9569c2090d5
Mark Shevelev <lionishy@gmail.com>
2021-08-02 20:06:29 +0300

    forward step kernel D coefficient calculation added

--- Diffusion/ForwardStepKernel.cuh
+++ Diffusion/ForwardStepKernel.cuh
@@ -36,4 +36,42 @@ namespace iki {
 			b(el_idx, ln_idx) = T(1) + (left + right);
 		}
 	}
+
+	template<typename T>
+	__global__ void forward_step_d_kernel(
+		DeviceArray<T, 2u> d,
+		DeviceArray<T, 2u> const f_curr,
+		DeviceArray<T, 2u> const dfc_along,
+		DeviceArray<T, 2u> const dfc_perp,
+		DeviceArray<T, 1u> const steps_along,
+		DeviceArray<T, 1u> const steps_perp,
+		T dt,
+		size_t const repeat
+	) { 
+		//number of the line to solve
+		unsigned ln_idx = blockIdx.x * blockDim.x + threadIdx.x;
+		if (0 == ln_idx || a.x_size - 1 <= ln_idx) return;
+
+		//index of the element in a line
+		unsigned el_idx_begin = blockIdx.y * blockDim.y * repeat + threadIdx.y;
+
+		for (unsigned el_cnt = 0; el_cnt != repeat; ++el_cnt) {
+			auto el_idx = el_idx_begin + blockDim.y * el_cnt;
+			if (0 == el_idx || a.y_size - 1 <= el_idx) continue;
+
+			auto left = dt * dfc_along(el_idx - 1, ln_idx) / steps_along(el_idx - 1) / (steps_along(el_idx - 1) + steps_along(el_idx));
+			auto right = dt * dfc_along(el_idx, ln_idx) / steps_along(el_idx) / (steps_along(el_idx - 1) + steps_along(el_idx));
+
+			auto up = 2 * dt * dfc_perp(el_idx, ln_idx - 1) / steps_perp(ln_idx - 1) / (steps_perp(ln_idx - 1) + steps_perp(ln_idx));
+			auto down = 2 * dt * dfc_perp(el_idx, ln_idx) / steps_perp(ln_idx) / (steps_perp(ln_idx - 1) + steps_perp(ln_idx));
+
+			d(el_idx, ln_idx) = f_curr(el_idx, ln_idx)
+				+ left * f_curr(el_idx - 1, ln_idx)
+				+ right * f_curr(el_idx + 1, ln_idx)
+				- (left + right) * f_curr(el_idx, ln_idx)
+				+ up * f_curr(el_idx, ln_idx - 1)
+				+ down * f_curr(el_idx, ln_idx + 1)
+				- (up + down) * f_curr(el_idx, ln_idx);
+		}
+	}
 }/*iki*/
