2692ec53465572d851e61174c871317323efaa4c
Mark Shevelev <lionishy@gmail.com>
2021-08-06 18:58:18 +0300

    Forward step correction kernel added

---
+++ Diffusion/ForwardStepCorrectionKernel.cuh
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <Array/DeviceArray.cuh>
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+namespace iki {
+	template<typename T>
+	__global__ void forward_step_correction_kernel(
+		DeviceArray<T, 2u> a,
+		DeviceArray<T, 2u> b,
+		DeviceArray<T, 2u> c,
+		DeviceArray<T, 2u> d,
+		DeviceArray<T, 2u> const f_next_tr,
+		DeviceArray<T, 2u> const f_curr_tr,
+		DeviceArray<T, 2u> const dfc_perp_tr,
+		DeviceArray<T, 1u> const steps_perp,
+		T dt,
+		size_t const repeat
+	) {
+		//number of the line to solve
+		unsigned ln_idx = blockIdx.x * blockDim.x + threadIdx.x;
+		if (0 == ln_idx || a.x_size - 1 <= ln_idx) return;
+
+		//index of the element in a line
+		unsigned el_idx_begin = blockIdx.y * blockDim.y * repeat + threadIdx.y;
+
+		for (unsigned el_cnt = 0; el_cnt != repeat; ++el_cnt) {
+			auto el_idx = el_idx_begin + blockDim.y * el_cnt;
+			if (0 == el_idx || a.y_size - 1 <= el_idx) continue;
+
+			auto left = dt * dfc_perp_tr(el_idx - 1, ln_idx) / steps_perp(el_idx - 1) / (steps_perp(el_idx - 1) + steps_perp(el_idx));
+			auto right = dt * dfc_perp_tr(el_idx, ln_idx) / steps_perp(el_idx) / (steps_perp(el_idx - 1) + steps_perp(el_idx));
+
+			a(el_idx, ln_idx) = -left;
+			c(el_idx, ln_idx) = -right;
+			b(el_idx, ln_idx) = T(1) + (left + right);
+
+			d(el_idx, ln_idx) = f_next_tr(el_idx, ln_idx)
+				- left * f_curr_tr(el_idx - 1, ln_idx)
+				- right * f_curr_tr(el_idx + 1, ln_idx)
+				+ (left + right) * f_curr_tr(el_idx, ln_idx);
+		}
+	}
+}/*iki*/
