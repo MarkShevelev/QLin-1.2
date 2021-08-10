c783eb36da2809efaeee8325edff9de0dde13c05
Mark Shevelev <lionishy@gmail.com>
2021-08-10 17:46:07 +0300

    transfer simplified

--- Array/Transfer.cuh
+++ Array/Transfer.cuh
@@ -23,13 +23,7 @@ namespace iki {
 
 	template <typename T, size_t Dim>
 	void transfer(iki::HostArray<T, Dim> const &from, iki::HostManagedDeviceArray<T, Dim> &to) {
-		if (from.get_full_size() != to.get_full_size())
-			throw std::runtime_error("can't transfer data: arrays of different sizes");
-		{
-			cudaError_t cudaStatus;
-			if (cudaSuccess != (cudaStatus = cudaMemcpy(to.data(), from.data(), from.get_full_size() * sizeof(T), cudaMemcpyHostToDevice)))
-				throw iki::DeviceError("can't transfer data from host to device: ", cudaStatus);
-		}
+		transfer(from, to.array());
 	}
 
 	template <typename T, size_t Dim>
@@ -45,12 +39,6 @@ namespace iki {
 
 	template <typename T, size_t Dim>
 	void transfer(iki::HostManagedDeviceArray<T, Dim> const &from, iki::HostArray<T, Dim> &to) {
-		if (from.get_full_size() != to.get_full_size())
-			throw std::runtime_error("can't transfer data: arrays of different sizes");
-		{
-			cudaError_t cudaStatus;
-			if (cudaSuccess != (cudaStatus = cudaMemcpy(to.data(), from.data(), from.get_full_size() * sizeof(T), cudaMemcpyDeviceToHost)))
-				throw iki::DeviceError("can't transfer data from device to host: ", cudaStatus);
-		}
+		transfer(from.array(), to);
 	}
 }/*iki*/
