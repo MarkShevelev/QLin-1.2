876eaefea909241b6927f9abb5db7143afd8a445
Mark Shevelev <lionishy@gmail.com>
2021-08-10 17:48:10 +0300

    backward kernel added

--- Solver/DeviceThomsonSweep.cuh
+++ Solver/DeviceThomsonSweep.cuh
@@ -7,7 +7,7 @@
 
 namespace iki {
 	template<typename T>
-	__global__ void thomson_sweep_kernel(
+	__global__ void thomson_sweep_forward_kernel(
 		iki::DeviceArray<T, 2u> a,
 		iki::DeviceArray<T, 2u> b,
 		iki::DeviceArray<T, 2u> c,
@@ -27,6 +27,30 @@ namespace iki {
 
 		for (size_t y_idx = y_size - 3; y_idx != 0; --y_idx) {
 			res(y_idx, x_idx) = fma(-c(y_idx, x_idx), res(y_idx + 1, x_idx), d(y_idx, x_idx)) / b(y_idx, x_idx);
+		}
+	}
+
+	template<typename T>
+	__global__ void thomson_sweep_backward_kernel(
+		iki::DeviceArray<T, 2u> a,
+		iki::DeviceArray<T, 2u> b,
+		iki::DeviceArray<T, 2u> c,
+		iki::DeviceArray<T, 2u> d,
+		iki::DeviceArray<T, 2u> res
+	) {
+		size_t x_size = res.x_size, y_size = res.y_size;
+		size_t x_idx = threadIdx.x + blockIdx.x * blockDim.x;
+		if (x_idx == 0 || x_idx >= x_size - 1) return;
+
+		for (size_t y_idx = y_size - 3; y_idx != 0; --y_idx) {
+			T w = c(y_idx, x_idx) / b(y_idx + 1, x_idx);
+			b(y_idx, x_idx) = fma(-w, a(y_idx + 1, x_idx), b(y_idx, x_idx));
+			d(y_idx, x_idx) = fma(-w, d(y_idx + 1, x_idx), d(y_idx, x_idx));
+		}
+		res(1, x_idx) = d(1, x_idx) / b(1, x_idx);
+
+		for (size_t y_idx = 2; y_idx != y_size - 1; ++y_idx) {
+			res(y_idx, x_idx) = fma(-a(y_idx, x_idx), res(y_idx - 1, x_idx), d(y_idx, x_idx)) / b(y_idx, x_idx);
 		}
 	}
 }/*iki*/
